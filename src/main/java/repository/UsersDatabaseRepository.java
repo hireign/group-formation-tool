345db8fe9eb6e95bb819fe05fdba314145f447f2
Joshin Johnson <js408882@dal.ca>
2020-06-04 06:00:21 -0300

    Login and signup mail

--- src/main/java/repository/UsersDatabaseRepository.java
+++ src/main/java/repository/UsersDatabaseRepository.java
@@ -3,6 +3,8 @@ package repository;
 import java.util.List;
 
 import dao.User;
+import dao.UserAccountStatus;
+import dao.UserLogin;
 import exception.CopyCatMeDBConfigException;
 import exception.CourseGroupFormationException;
 import util.IPasswordEncryption;
@@ -14,4 +16,12 @@ public interface UsersDatabaseRepository {
 	public List<User> getAllUsers() throws CopyCatMeDBConfigException, CourseGroupFormationException;
 	
 	public User fetchByBannerId(String bannerId) throws CopyCatMeDBConfigException, CourseGroupFormationException;
+	
+	public boolean getUserLogin(UserLogin userLogin);
+
+	public String getUserRole(String username);
+
+	public void saveAccountStatus(UserAccountStatus uas);
+
+	boolean verifyUser(String token, int i);
 }
