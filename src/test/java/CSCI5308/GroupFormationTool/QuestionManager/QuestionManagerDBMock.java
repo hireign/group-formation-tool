67d7cd8fbc200eac20c172cb7d3e4867e497a165
Hirenkumar Khant <hr266981@dal.ca>
2020-06-10 21:28:55 -0300

    added test package and mock for QuestionManagerTest

---
+++ src/test/java/CSCI5308/GroupFormationTool/QuestionManager/QuestionManagerDBMock.java
@@ -0,0 +1,17 @@
+package CSCI5308.GroupFormationTool.QuestionManager;
+
+import java.util.List;
+
+public class QuestionManagerDBMock {
+    public List<Question> loadAllQuestionsByInstructor(int instructorId) {
+        return null;
+    }
+
+    public boolean createQuestion(Question question) {
+        return false;
+    }
+
+    public boolean deleteQuestion(int id) {
+        return false;
+    }
+}
